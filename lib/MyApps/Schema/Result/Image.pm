49b7af48a766e9b5988a32dec9693f565959541c
Hiroaki Asano <hiro_asano@r.recruit.co.jp>
2013-05-28 14:56:03 +0900

    add Image model

---
+++ lib/MyApps/Schema/Result/Image.pm
@@ -0,0 +1,102 @@
+use utf8;
+package MyApps::Schema::Result::Image;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+MyApps::Schema::Result::Image
+
+=cut
+
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use MooseX::MarkAsMethods autoclean => 1;
+extends 'DBIx::Class::Core';
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=back
+
+=cut
+
+__PACKAGE__->load_components("InflateColumn::DateTime");
+
+=head1 TABLE: C<images>
+
+=cut
+
+__PACKAGE__->table("images");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 gid
+
+  data_type: 'integer'
+  is_nullable: 1
+
+=head2 path
+
+  data_type: 'varchar'
+  is_nullable: 1
+  size: 256
+
+=head2 posted_at
+
+  data_type: 'timestamp'
+  datetime_undef_if_invalid: 1
+  default_value: current_timestamp
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "gid",
+  { data_type => "integer", is_nullable => 1 },
+  "path",
+  { data_type => "varchar", is_nullable => 1, size => 256 },
+  "posted_at",
+  {
+    data_type => "timestamp",
+    datetime_undef_if_invalid => 1,
+    default_value => \"current_timestamp",
+    is_nullable => 0,
+  },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+
+# Created by DBIx::Class::Schema::Loader v0.07035 @ 2013-05-28 14:48:46
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:RIy8uM6fB6mcaNFziaNm8w
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+__PACKAGE__->meta->make_immutable;
+1;

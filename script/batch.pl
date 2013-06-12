23eedd9a4efaf32286e5a04b3043ff2ed97d4093
mokemoko <mokemoko331@gmai.com>
2013-06-12 12:54:33 +0900

    add batch action to get image

--- script/batch.pl
+++ script/batch.pl
@@ -1,10 +1,10 @@
 use warnings;
 use strict;
-use MyApp;
+use FindBin;
+use lib "$FindBin::RealBin/../lib";
+use MyApps;
 use Catalyst::Stats;
 
-# バッチ処理用スクリプト
-
-my $app = MyApp->new( stats => Catayst::Stats->new );
-my $res = $app->forward( '/path', [ "params" ] );
-print $res;
+my $app = MyApps->new(stats => Catalyst::Stats->new);
+my @args = [];
+my $res = $app->forward('/batch/getImages', @args);

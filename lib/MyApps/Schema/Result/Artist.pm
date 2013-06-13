91f52edb329a7eb9fc376a6436403c15a87d407d
mokemoko <mokemoko331@gmai.com>
2013-06-13 18:16:07 +0900

    create relation between images and artists model.

--- lib/MyApps/Schema/Result/Artist.pm
+++ lib/MyApps/Schema/Result/Artist.pm
@@ -104,4 +104,7 @@ __PACKAGE__->set_primary_key("id");
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
+
+__PACKAGE__->has_many(images => 'MyApps::Schema::Result::Image', 'aid');
+
 1;
